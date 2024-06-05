aa40d24bb2d88ec39c9d4f6d3a98a4b661f51fa2
SachaBouhey <sacha@lolice.xyz>
2024-06-05 09:10:51 +0200

    Some testings

--- src/main/java/lolice/xyz/Skills.java
+++ src/main/java/lolice/xyz/Skills.java
@@ -6,10 +6,42 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Skills {
-    public static void SkillsInit(String[] args) {
-        Map<String, ArrayList> skills = new HashMap<>();
-        skills.put("MageSkill" , new ArrayList<>());
-        skills.get("MageSkill").add("test");
-        skills.get("MageSkill").add(1);
+    public String SkillName;
+    public String Description;
+    public int Damage;
+    public boolean IsActive;
+
+    public Skills(String SkillName, String Description, int Damage, boolean IsActive) {
+        this.SkillName = SkillName;
+        this.Description = Description;
+        this.Damage = Damage;
+        this.IsActive = IsActive;
+    }
+
+    //Getters
+    public String getSkillName() {
+        return SkillName;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public int getDamage() {
+        return Damage;
+    }
+
+    public boolean isActive() {
+        return IsActive;
+    }
+
+    //Setters
+
+    public void setActive() {
+        this.IsActive = true;
+    }
+
+    public void useSkill(String SkillName) {
+
     }
 }
