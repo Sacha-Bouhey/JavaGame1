dbdb063ccaa186c93fd285efe0eb4f0b3aa4fd17
trololol42 <sac.bouhey@gmail.com>
2024-06-11 18:23:34 +0200

    Finishing the object

--- src/main/java/lolice/xyz/Enemy_init.java
+++ src/main/java/lolice/xyz/Enemy_init.java
@@ -1,10 +1,70 @@
+
 package lolice.xyz;
 
-import java.util.ArrayList;
-import java.util.Map;
+import java.util.List;
 
 public class Enemy_init {
-    public static Enemy_init(Map <String, ArrayList<Enemy>> default_class_value) {
+    private String name;
+    private int health;
+    private int strength;
+    private int mana;
+    private int defence;
+    private List<Skill> skills;
 
+    public Enemy_init(String Ename, int Ehealth, int Estrength, int Emana, int Edefence, List<Skill> Eskills) {
+        this.name = Ename;
+        this.health = Ehealth;
+        this.strength = Estrength;
+        this.mana = Emana;
+        this.defence = Edefence;
+        this.skills = Eskills;
+    }
+    //Getters
+    public String getName() {
+        return name;
+    }
+    public int getHealth() {
+        return health;
+    }
+    public int getStrength() {
+        return strength;
+    }
+    public int getMana() {
+        return mana;
+    }
+    public int getDefence() {
+        return defence;
+    }
+    public List<Skill> getSkills() {
+        return skills;
+    }
+
+    //Setters
+    public void setHealth(int newhealth) {
+        this.health = newhealth;
+    }
+    public void setStrength(int newstrength) {
+        this.strength = newstrength;
+    }
+    public void setMana(int newmana) {
+        this.mana = newmana;
+    }
+    public void setDefence(int newdefence) {
+        this.defence = newdefence;
+    }
+    public void takeDamage(int damage) {
+        this.health -= damage - defence;
+    }
+    //showInfo
+    public void showInfo() {
+        System.out.println("Name: " + this.name);
+        System.out.println("Health: " + this.health);
+        System.out.println("Strength: " + this.strength);
+        System.out.println("Mana: " + this.mana);
+        System.out.println("Defence: " + this.defence);
+        System.out.println("Skills: ");
+        for (Skill skill : this.skills) {
+            System.out.println(skill.getSkillName());
+        }
     }
 }
