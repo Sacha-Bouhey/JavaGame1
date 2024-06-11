f9640007e14ee58f54faea5e7c39d3f8ec5b672d
trololol42 <sac.bouhey@gmail.com>
2024-06-11 18:24:17 +0200

    Adding battle start for testing purposes

--- src/main/java/lolice/xyz/Main.java
+++ src/main/java/lolice/xyz/Main.java
@@ -4,6 +4,12 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Characters.InitCharValue(args);
+        //Init player and enemy
+        Characters_init player = Player_init.Player_init(Characters.InitCharValue(args), Player_choice.GetClassName(args));
+        Enemy_init enemy = Enemy_skill_init.Enemy_skill_init(Enemy.InitEnemyValue(args), "Gobelin");
+        //Start battle
+        Battle battle = new Battle(player, enemy);
+        battle.Start();
+
     }
 }
