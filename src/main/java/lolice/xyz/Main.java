7eb05c9d829956f4ea411bc0ee52408efa95676b
SachaBouhey <sacha@lolice.xyz>
2024-06-17 16:54:10 +0200

    Added info for further tests

--- src/main/java/lolice/xyz/Main.java
+++ src/main/java/lolice/xyz/Main.java
@@ -4,18 +4,18 @@ package lolice.xyz;
 import lolice.xyz.Enemies.Enemy;
 import lolice.xyz.Enemies.Enemy_init;
 import lolice.xyz.Enemies.Enemy_skill_init;
-import lolice.xyz.Players.Characters;
-import lolice.xyz.Players.Characters_init;
-import lolice.xyz.Players.Player_choice;
-import lolice.xyz.Players.Player_init;
+import lolice.xyz.Players.*;
 
 public class Main {
     public static void main(String[] args) {
         //Init player and enemy
         Characters_init player = Player_init.Player_init(Characters.InitCharValue(), Player_choice.GetClassName(args));
-        Enemy_init enemy = Enemy_skill_init.Enemy_skill_init(Enemy.InitEnemyValue(), "Goblin");
+        Enemy_init enemy = Enemy_skill_init.Enemies_skill_init(Enemy.InitEnemyValue(), "Goblin");
+        player.showInfo();
+        player.showInfo();
         //Start battle
         Battle battle = new Battle(player, enemy);
         battle.Start();
+        player.showInfo();
     }
 }
