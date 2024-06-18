201476085331b7247301667bd4df583260679d47
SachaBouhey <sacha@lolice.xyz>
2024-06-18 16:48:51 +0200

    Some menu

---
+++ src/main/java/lolice/xyz/Iddle/Menu.java
@@ -0,0 +1,57 @@
+package lolice.xyz.Iddle;
+import lolice.xyz.Players.Characters_init;
+import java.util.Scanner;
+
+public class Menu {
+    public static void startMenu() {
+        while (true) {
+            Scanner UserChoice = new Scanner(System.in);
+            System.out.println("Welcome to lolo's RPG");
+            System.out.println("1. Start game");
+            System.out.println("2. Exit");
+            System.out.println("Enter your choice: ");
+            int Choice = UserChoice.nextInt();
+            if (Choice == 1) {
+                System.out.println("Game started");
+                return;
+            } else if (Choice == 2) {
+                System.out.println("Game exited, goodbye !");
+                System.exit(0);
+            } else {
+                System.out.println("Invalid choice");
+            }
+        }
+    }
+    public static void iddleMenu(Characters_init player) {
+        while (true) {
+            System.out.println("What do you want to do?");
+            Scanner UserChoice = new Scanner(System.in);
+            System.out.println("1. Explore");
+            System.out.println("2. Show current stats");
+            System.out.println("3. Show current location");
+            System.out.println("4. Show player inventory");
+            System.out.println("0. Return to main menu");
+            System.out.println("Enter your choice: ");
+            int Choice = UserChoice.nextInt();
+            if (Choice == 1) {
+                System.out.println("Explore");
+            }
+            else if (Choice == 2) {
+                player.showInfo();
+            }
+            else if (Choice == 3) {
+                System.out.println("Show current location");
+            }
+            else if (Choice == 4) {
+                System.out.println("Show current inventory");
+            }
+            else if (Choice == 0) {
+                System.out.println("Returning to main menu...");
+                startMenu();
+            }
+            else {
+                System.out.println("Invalid choice");
+            }
+        }
+    }
+}
