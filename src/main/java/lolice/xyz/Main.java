5689bab4fdd8a65c50f23880d9bf9ed815cb8b94
SachaBouhey <sacha@lolice.xyz>
2024-06-21 16:59:20 +0200

    Main now only have 1 line to start the program

--- src/main/java/lolice/xyz/Main.java
+++ src/main/java/lolice/xyz/Main.java
@@ -4,17 +4,15 @@ package lolice.xyz;
 import lolice.xyz.Enemies.Enemy;
 import lolice.xyz.Enemies.Enemy_init;
 import lolice.xyz.Enemies.Enemy_skill_init;
+import lolice.xyz.Iddle.Game_Start;
+import lolice.xyz.Iddle.Menu;
 import lolice.xyz.Players.*;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         //Init player and enemy
-        Characters_init player = Player_init.Player_init(Characters.InitCharValue(), Player_choice.GetClassName());
-        Enemy_init enemy = Enemy_skill_init.Enemies_skill_init(Enemy.InitEnemyValue(), "Goblin");
-        //Start battle
-        player.showInfo();
-        Battle battle = new Battle(player, enemy);
-        battle.Start();
-        player.showInfo();
+        Game_Start.lore();
     }
 }
