19da51512dfb4c8819122686c3ead1b54978163a
SachaBouhey <sacha@lolice.xyz>
2024-06-21 16:59:38 +0200

    Modifying the menu to be usable with the current configuration

--- src/main/java/lolice/xyz/Iddle/Menu.java
+++ src/main/java/lolice/xyz/Iddle/Menu.java
@@ -1,11 +1,19 @@
 package lolice.xyz.Iddle;
 import lolice.xyz.Players.Characters_init;
+import lolice.xyz.Skill;
 import lolice.xyz.Skill_stats;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Menu {
-    public static void startMenu() {
+    private final Characters_init player;
+
+    public Menu(Characters_init player) {
+        this.player = player;
+    }
+
+    public static void startMenu(Characters_init player) {
         while (true) {
             Scanner UserChoice = new Scanner(System.in);
             System.out.println("Welcome to lolo's RPG");
@@ -15,7 +23,7 @@ public class Menu {
             int Choice = UserChoice.nextInt();
             if (Choice == 1) {
                 System.out.println("Game started");
-                return;
+                iddleMenu(player);
             } else if (Choice == 2) {
                 System.out.println("Game exited, goodbye !");
                 System.exit(0);
@@ -24,6 +32,15 @@ public class Menu {
             }
         }
     }
+
+    private Skill_stats getSkill_statsByName(String skill_stats_name){
+        for(Skill_stats skill_stats : player.getSkill_stats()) {
+            if(skill_stats.getSkill_name().equals(skill_stats_name)) {
+                return skill_stats;
+            }
+        }return null;
+    }
+
     public static void iddleMenu(Characters_init player) {
         while (true) {
             System.out.println("What do you want to do?");
@@ -49,20 +66,41 @@ public class Menu {
             }
             else if (Choice == 0) {
                 System.out.println("Returning to main menu...");
-                startMenu();
+                startMenu(player);
             }
             else {
                 System.out.println("Invalid choice");
             }
         }
     }
-    public static void playerSkillStatsUpgrade(Skill_stats skill_stats) {
+    public void playerSkillStatsUpgrade() {
         while (true) {
             System.out.println("What do you want to upgrade?");
             Scanner UserChoice = new Scanner(System.in);
             System.out.println("1. Staff");
             System.out.println("2. Books");
             System.out.println("3. Magic gauntlet ");
+            System.out.println("0. Return to main menu");
+            System.out.println("Enter your choice: ");
+            int Choice = UserChoice.nextInt();
+            if (Choice == 1) {
+                Skill_stats skill_stats = getSkill_statsByName("Staff");
+                skill_stats.upgradeSkillStats(skill_stats, player);
+                break;
+            }
+            else if(Choice == 2) {
+                Skill_stats skill_stats = getSkill_statsByName("Books");
+                skill_stats.upgradeSkillStats(skill_stats, player);
+                break;
+            } else if (Choice == 3) {
+                Skill_stats skill_stats = getSkill_statsByName("Magic gauntlet");
+                skill_stats.upgradeSkillStats(skill_stats, player);
+                break;
+            }
+            else{
+                System.out.println("Invalid choice");
+                break;
+            }
         }
     }
 }
