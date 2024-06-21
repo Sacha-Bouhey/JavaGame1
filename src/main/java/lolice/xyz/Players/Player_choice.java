7f8cc0a19d0e4486e0fad6bb6811f652e96224a6
SachaBouhey <sacha@lolice.xyz>
2024-06-21 16:59:53 +0200

    Finishing Playerchoice

--- src/main/java/lolice/xyz/Players/Player_choice.java
+++ src/main/java/lolice/xyz/Players/Player_choice.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Player_choice  {
     public static String GetClassName() {
         Scanner UserClass = new Scanner(System.in);
+
         System.out.println("Enter your class: ");
         String ClassName = UserClass.nextLine();
         return ClassName;
