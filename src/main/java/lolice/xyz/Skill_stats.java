97fcbd823b1f173f65bef134bbb3ea726ff5d54a
SachaBouhey <sacha@lolice.xyz>
2024-06-21 17:00:28 +0200

    Adding a classname to ,reconized what class it uses

--- src/main/java/lolice/xyz/Skill_stats.java
+++ src/main/java/lolice/xyz/Skill_stats.java
@@ -1,18 +1,44 @@
 package lolice.xyz;
 
-public class Skill_stats {
-    public String skill_name;
-    public int skill_level;
+import lolice.xyz.Players.Characters_init;
 
-    public Skill_stats(String skill_name, int skill_level) {
+public class Skill_stats {
+    private String skill_name;
+    private String class_name;
+    private int skill_level;
+
+    public Skill_stats(String skill_name, String class_name, int skill_level) {
         this.skill_name = skill_name;
-        this.skill_level = skill_level;
+        this.class_name = class_name;
+        this.skill_level = 0;
     }
+
     public String getSkill_name() {
         return skill_name;
     }
-    public int getSkill_stats() {
+    public String getClass_name() {
+        return class_name;
+    }
+    public int getSkill_level() {
         return skill_level;
+    }
+    public void setSkill_stats(int new_skill_level) {
+        this.skill_level = new_skill_level;
+    }
+
+    public void showSkillStats() {
+        System.out.println("Skill name: " + this.skill_name + " Skill level: " + this.skill_level);
+    }
+
+    public void upgradeSkillStats(Skill_stats skill_stats, Characters_init player) {
+        if(player.getStatpoint() > 0) {
+            player.setStatpoint(player.getStatpoint() -1);
+            skill_stats.setSkill_stats(skill_stats.getSkill_level() + 1);
+            System.out.println("Skill upgraded !");
+        }
+        else {
+            System.out.println("Not enough stat points");
+        }
     }
 
 }
