a30b7ba88add9b9c782746d90c00055638220f4b
SachaBouhey <sacha@lolice.xyz>
2024-07-23 15:21:22 +0200

    Added map to initialize location object.

--- src/main/java/lolice/xyz/Iddle/Game_Start.java
+++ src/main/java/lolice/xyz/Iddle/Game_Start.java
@@ -1,13 +1,28 @@
 package lolice.xyz.Iddle;
 
+import lolice.xyz.Location;
 import lolice.xyz.Players.Characters;
 import lolice.xyz.Players.Characters_init;
 import lolice.xyz.Players.Player_choice;
 import lolice.xyz.Players.Player_init;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Game_Start {
+    public static Map <String, Location> Location_init = new HashMap<>();
+    public static void main(String[] args) {
+        initLocation();
+        lore();
+    }
+    public static void initLocation() {
+        Location_init = new HashMap<>();
+        Location_init.put("Starting village", new Location("Stating village", 0, 0, true));
+        Location_init.put("Forest", new Location("Forest", 1, 0, false));
+        Location_init.put("Cave", new Location("Cave", 0, 1, false));
+        Location_init.put("Castle", new Location("Castle", 0, -1, false));
+    }
     public static void lore() {
         System.out.println("You woke up in a field, and you have a mission. You know it, but you can't remember...");
         System.out.println("You just remember something ! You were a: ");
@@ -26,6 +41,7 @@ public class Game_Start {
         }
         System.out.println("Yes ! you were a: " + player.getName() + "! You can finally start your journey");
         System.out.println("Good luck !");
-        Menu.startMenu(player);
+        Menu menu = new Menu(player);
+        menu.showStartMenu();
     }
 }
