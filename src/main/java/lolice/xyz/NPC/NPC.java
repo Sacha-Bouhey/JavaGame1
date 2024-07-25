4e330a6720c1ffc8b9ed8b732827e423c80c7eae
SachaBouhey <sacha@lolice.xyz>
2024-07-25 16:37:13 +0200

    Modified existing code for new system.

--- src/main/java/lolice/xyz/NPC/NPC.java
+++ src/main/java/lolice/xyz/NPC/NPC.java
@@ -1,13 +1,16 @@
 package lolice.xyz.NPC;
 
+import lolice.xyz.Players.Characters_init;
+
 public class NPC {
     private final String name;
     private int hp;
     private int damage;
     private int gold;
     private String dialog;
+    private Quest quest;
 
-    public NPC(String name, int hp, int damage, int level, int exp, int gold, String dialog) {
+    public NPC(String name, int hp, int damage, int level, int gold, String dialog) {
         this.name = name;
         this.hp = hp;
         this.damage = damage;
@@ -31,8 +34,13 @@ public class NPC {
     public int getGold() {
         return gold;
     }
+
     public String getDialog() {
         return dialog;
+    }
+
+    public Quest getQuest() {
+        return quest;
     }
 
     //setters
@@ -52,6 +60,28 @@ public class NPC {
         System.out.println("Name: " + name);
         System.out.println("HP: " + hp);
         System.out.println("Damage: " + damage);
-        System.out.println("Gold: " + gold);
+        System.out.println("Gold: " + gold + "\n");
+    }
+
+    public void showNPCQuest() {
+        quest.showQuestInfo();
+    }
+
+    public void addQuest(Quest quest) {
+        this.quest = quest;
+    }
+
+    public void questAccepted(Characters_init player) {
+        System.out.println("Quest accepted: " + quest.getName());
+        player.addActiveQuest(quest);
+        questUnlockable(quest,player);
+    }
+
+    public void questUnlockable(Quest quest,Characters_init player) {
+        if (quest.getName().equals("Tutorial 1: Defeat Goblin")) {
+            System.out.println("You just unlocked the forest location! \n You can go there with the explore option to find and defeat goblins." +
+                    " \n You might find something usefull there! Good luck! \n");
+            player.unlockLocation("Forest");
+        }
     }
 }
