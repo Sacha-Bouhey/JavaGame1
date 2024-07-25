58e3625b55dbfb97de057b4f2c6c3885a8495f8f
SachaBouhey <sacha@lolice.xyz>
2024-07-25 16:35:21 +0200

    Added test initEnemies and initNPC and modified initLocation for new child system

--- src/main/java/lolice/xyz/Iddle/Game_Start.java
+++ src/main/java/lolice/xyz/Iddle/Game_Start.java
@@ -7,28 +7,48 @@ import lolice.xyz.Players.Player_choice;
 import lolice.xyz.Players.Player_init;
 import lolice.xyz.NPC.Quest;
 import lolice.xyz.NPC.NPC;
+import lolice.xyz.Enemies.Enemy_init;
+import lolice.xyz.Skill;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
 public class Game_Start {
-    public static Map <String, Location> Location_init = new HashMap<>();
+    public static Map <String, NPC> NPC_init = new HashMap<>();
+    public static Map <String, Quest> Quest_init = new HashMap<>();
+    public static List<Location> Location_init = new ArrayList<>();
+    public static Map <String, List<Enemy_init>> Enemies_init = new HashMap<>();
     public static void main(String[] args) {
-        initLocation();
+        initEnemies();
         initNPC();
+        initLocation();
         lore();
     }
 
+    public static void initEnemies() {
+        List<Skill> goblinSkills = new ArrayList<>();
+        goblinSkills.add(new Skill("Goblin claw", "Weak attack, but dangerous for a beginner", 10, 0, true));
+        List <Enemy_init> enemies = new ArrayList<>();
+        enemies.add(new Enemy_init("Goblin", 100, 10, 1, 100, goblinSkills));
+        Enemies_init.put("Forest", enemies);
+    }
+
     public static void initLocation() {
-        Location_init = new HashMap<>();
-        Location_init.put("Starting village", new Location("Stating village", 0, 0, true, "The starting village is a peaceful place for beginners to start their journey."));
-        Location_init.put("Forest", new Location("Forest", 1, 0, false, "The forest is a dangerous place with monsters lurking around."));
-        Location_init.put("Cave", new Location("Cave", 0, 1, false, "The cave is a dark and mysterious place with hidden treasures."));
-        Location_init.put("Castle", new Location("Castle", 0, -1, false, "The castle is a place where the king resides and holds great power."));
+        Location_init.add(new Location.Village("Stating village", 0, 0, true, "The starting village is a peaceful place for beginners to start their journey."));
+        Location_init.add(new Location.Wilderness("Forest", 1, 0, false, "The forest is a dangerous place with monsters lurking around.", Enemies_init.get("Forest")));
+        Location_init.add(new Location("Cave", 0, 1, false, "The cave is a dark and mysterious place with hidden treasures."));
+        Location_init.add(new Location("Castle", 0, -1, false, "The castle is a place where the king resides and holds great power."));
     }
     public static void initNPC() {
-        Quest quests = new Quest("Defeat goblins","Defeat Goblin", 100, 10, "You must kill 10 goblins to complete this quest.", 1);
+        Quest_init.put("Tutorial 1",new Quest("Tutorial 1: Defeat Goblin","Defeat Goblin", 100, 10, "You must kill 2 goblins to complete this quest.", 2));
+
+        NPC_init.put("Tutorial villager", new NPC("Tutorial villager", 100, 10, 1,1000,"Hello adventurer! " +
+                "I have a quest for you. You must defeat 2 goblins to complete this quest. \n " + "You can find goblins in the forest. Good luck!"));
+
+        NPC_init.get("Tutorial villager").addQuest(Quest_init.get("Tutorial 1"));
     }
 
     public static void lore() {
@@ -49,6 +69,10 @@ public class Game_Start {
         }
         System.out.println("Yes ! you were a: " + player.getName() + "! You can finally start your journey");
         System.out.println("Good luck !");
+        player.setLocations(Location_init);
+        if (Location_init.getFirst() instanceof Location.Village) {
+            ((Location.Village) Location_init.getFirst()).addNPC(NPC_init.get("Tutorial villager"));
+        }
         Menu menu = new Menu(player);
         menu.showStartMenu();
     }
