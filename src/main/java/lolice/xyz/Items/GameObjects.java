a50637466f77c89ebcd3f0261854fbc017625454
SachaBouhey <sacha@lolice.xyz>
2024-08-05 14:19:12 +0200

    Added GameObjects to store all my items.

---
+++ src/main/java/lolice/xyz/Items/GameObjects.java
@@ -0,0 +1,6 @@
+package lolice.xyz.Items;
+
+import lolice.xyz.Items.Items;
+public class GameObjects {
+    public static final Items.Potion SMALL_HEALING_POTION = new Items.Potion("Small healing potion", "Heals you for 20 HP", 0, 0, false, 20, 0);
+}
