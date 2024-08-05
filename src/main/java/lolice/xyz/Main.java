2b8a582502fe569fe713a776cb86f7d61b7930c9
SachaBouhey <sacha@lolice.xyz>
2024-08-05 17:00:10 +0200

    Remove non-relatable comment

--- src/main/java/lolice/xyz/Main.java
+++ src/main/java/lolice/xyz/Main.java
@@ -4,7 +4,6 @@ import lolice.xyz.Iddle.Game;
 
 public class Main {
     public static void main(String[] args) {
-        //Init player and enemy
         Game game = new Game();
         game.start();
     }
