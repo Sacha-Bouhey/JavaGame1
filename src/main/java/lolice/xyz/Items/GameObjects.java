4f4e3d6d4c2bfe8476e5daabe85e56d1632f34ac
SachaBouhey <sacha@lolice.xyz>
2024-08-07 16:57:02 +0200

    Comments added for better view

--- src/main/java/lolice/xyz/Items/GameObjects.java
+++ src/main/java/lolice/xyz/Items/GameObjects.java
@@ -2,5 +2,9 @@ package lolice.xyz.Items;
 
 import lolice.xyz.Items.Items;
 public class GameObjects {
+    //potions
     public static final Items.Potion SMALL_HEALING_POTION = new Items.Potion("Small healing potion", "Heals you for 20 HP", 0, 0, false, 20, 0);
+
+    //keys
+
 }
